76b7bbe8385adbe1dd9ced1f11f129bb1ee40c6f
Fabio Dias <mfabiodias@gmail.com>
2020-12-12 19:09:26 -0300

    Bootstrap e Jquery adicionado e configurado

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home - Keller Imoveis</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <h1>Keller Imoveis</h1>
+
+    <button class="btn btn-lg btn-pink">Bootstrap OK</button>
+
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+</body>
+</html>
